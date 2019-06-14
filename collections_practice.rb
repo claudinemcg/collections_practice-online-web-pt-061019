9849e6658badca50ea8fc5838447f9298202a366
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-14 13:11:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.select do |word|
+    a.length <=> b.length
+  end
+end
+
+
+
+def swap_elements
+  
+end
 
